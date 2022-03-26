63f6fd429aa4e3a58ba783c40522e38c495da7b6
Ezbim <bhima.pilot@gmail.com>
2022-03-26 11:36:34 +0700

    adding the message and some align fix

--- Bjava.js
+++ Bjava.js
@@ -1,10 +1,43 @@
 //3BAR TOGGLE
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.nav__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav__menu');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+    
 });
 
+const mseg = document.querySelector('#mesage');
+const msegLinks = document.querySelector('.msg');
+const bground = document.querySelector('.hero');
+const xbut = document.querySelector('.close')
 
+xbut.addEventListener('click',function(){
+    msegLinks.classList.toggle('active');
+    bground.classList.toggle('active');
+    document.getElementById('mbox').value='';
+    document.getElementById('mname').value='';
+});
+mseg.addEventListener('click', function() {
+    msegLinks.classList.toggle('active');
+    bground.classList.toggle('active');
+    
+});
+
+ 
+function send() {
+    var z = document.querySelector('#mname').value;
+    var y = document.querySelector('#mbox').value;
+    if((z == "")||( y == "")) {
+    alert("MUST FILL ALL THE BOX OK :)");
+    
+    return false
+    }else if(z != "" && y != ""){
+    mseg.classList.toggle('is-active');
+    msegLinks.classList.toggle('active');
+    bground.classList.toggle('active');
+    document.getElementById('mbox').value='';
+    document.getElementById('mname').value='';
+    }
+} 
