7999ef79efeac600b317c874c6748bd23ae6fac7
Ezbim <bhima.pilot@gmail.com>
2022-04-06 11:12:45 +0700

    adding Light/Dark Mode

--- Bjava.js
+++ Bjava.js
@@ -15,18 +15,46 @@ const xbut = document.querySelector('.close');
 
 xbut.addEventListener('click',function(){
     msegLinks.classList.toggle('active');
-    bground.classList.toggle('active');
     document.getElementById('mbox').value='';
     document.getElementById('mname').value='';
 });
 mseg.addEventListener('click', function() {
     msegLinks.classList.toggle('active');
-    bground.classList.toggle('active');
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
     
 });
 
+const light = document.getElementById('lightmode')
+const tog = document.getElementById('radiant3').value='1';
+light.onclick = function (){
+    document.body.classList.toggle('light-mode')
+    
+    if (document.getElementById('radiant3').value=='1')
+    {
+        document.getElementById('radiant3').value='3';
+        document.getElementById('radmobile').src="./assets/Logo/SVG2/radaja2.svg";
+        document.getElementById('radiant1').src="./assets/Logo/SVG2/Aset 1.svg";
+        document.getElementById('radiant3').src="./assets/Logo/SVG2/Aset 3.svg";
+        document.getElementById('radiant4').src="./assets/Logo/SVG2/Aset 4.svg";
+        document.getElementById('radiant5').src="./assets/Logo/SVG2/Aset 5.svg";
+        document.getElementById('radiant6').src="./assets/Logo/SVG2/Aset 6.svg";
+        document.getElementById('radiant7').src="./assets/Logo/SVG2/Aset 7.svg";
+        document.getElementById('radiant8').src="./assets/Logo/SVG2/Aset 8.svg";
+        document.getElementById('radiant9').src="./assets/Logo/SVG2/Aset 9.svg";
+    }else{
+        document.getElementById('radiant3').value='1';
+        document.getElementById('radmobile').src="./assets/Logo/SVG/radaja.svg";
+        document.getElementById('radiant1').src="./assets/Logo/SVG/Asset 1.svg";
+        document.getElementById('radiant3').src="./assets/Logo/SVG/Asset 3.svg";
+        document.getElementById('radiant4').src="./assets/Logo/SVG/Asset 4.svg";
+        document.getElementById('radiant5').src="./assets/Logo/SVG/Asset 5.svg";
+        document.getElementById('radiant6').src="./assets/Logo/SVG/Asset 6.svg";
+        document.getElementById('radiant7').src="./assets/Logo/SVG/Asset 7.svg";
+        document.getElementById('radiant8').src="./assets/Logo/SVG/Asset 8.svg";
+        document.getElementById('radiant9').src="./assets/Logo/SVG/Asset 9.svg";
+    }
+}
  
 function send() {
     var z = document.querySelector('#mname').value;
